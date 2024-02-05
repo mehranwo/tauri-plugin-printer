7ebb86bdbe4d5483ae0a49d40375b2c2750bfaca
me.dr7725@gmail.com <me.dr7725@gmail.com>
2024-02-05 10:51:04 +0330

    change tauri with std:process

--- src/windows.rs
+++ src/windows.rs
@@ -1,31 +1,28 @@
-
-
-// use std::process::{Command, Stdio};
-use std::{sync::mpsc, io::Write};
-use std::thread;
-use std::fs::File;
-use std::env;
-use tauri::api::process::Command;
 use crate::{declare::PrintOptions, fsys::remove_file};
+use std::env;
+use std::fs::File;
+use std::process::{Command, Stdio};
+use std::thread;
+use std::{io::Write, sync::mpsc};
+// use tauri::core::process::Command;
 /**
  * Create sm.exe to temp
  */
 fn create_file(path: String, bin: &[u8]) -> std::io::Result<()> {
     let mut f = File::create(format!("{}sm.exe", path))?;
     f.write_all(bin)?;
-  
+
     f.sync_all()?;
     Ok(())
 }
 
-  
 /**
  * init sm.exe
  */
 pub fn init_windows() {
     let sm = include_bytes!("bin/sm");
     let dir: std::path::PathBuf = env::temp_dir();
-    let result: Result<(), std::io::Error>  = create_file(dir.display().to_string(),sm);
+    let result: Result<(), std::io::Error> = create_file(dir.display().to_string(), sm);
     if result.is_err() {
         panic!("Gagal")
     }
@@ -40,7 +37,7 @@ pub fn get_printers() -> String {
 
     // Spawn a new thread
     thread::spawn(move || {
-        let output: tauri::api::process::Output = Command::new("powershell").args(["Get-Printer | Select-Object Name, DriverName, JobCount, PrintProcessor, PortName, ShareName, ComputerName, PrinterStatus, Shared, Type, Priority | ConvertTo-Json"]).output().unwrap();
+        let output: tauri::core::process::Output = Command::new("powershell").args(["Get-Printer | Select-Object Name, DriverName, JobCount, PrintProcessor, PortName, ShareName, ComputerName, PrinterStatus, Shared, Type, Priority | ConvertTo-Json"]).output().unwrap();
 
         sender.send(output.stdout.to_string()).unwrap();
     });
@@ -50,8 +47,7 @@ pub fn get_printers() -> String {
     // Receive the result from the spawned thread
     let result: String = receiver.recv().unwrap();
 
-
-    return result
+    return result;
 }
 
 /**
@@ -63,24 +59,32 @@ pub fn get_printers_by_name(printername: String) -> String {
 }
 
 /**
- * Print pdf file 
+ * Print pdf file
  */
-pub fn print_pdf (options: PrintOptions) -> String {
+pub fn print_pdf(options: PrintOptions) -> String {
     let dir: std::path::PathBuf = env::temp_dir();
     let print_setting: String = options.print_setting;
     let mut print: String = "-print-to-default".to_owned();
     if options.id.len() == 0 {
         print = format!("-print-to {}", options.id).to_owned();
     }
-    let shell_command = format!("{}sm.exe {} {} -silent {}", dir.display(), print, print_setting, options.path);
-    
+    let shell_command = format!(
+        "{}sm.exe {} {} -silent {}",
+        dir.display(),
+        print,
+        print_setting,
+        options.path
+    );
 
     // Create a channel for communication
     let (sender, receiver) = mpsc::channel();
     println!("{}", shell_command);
     // Spawn a new thread
     thread::spawn(move || {
-        let output: tauri::api::process::Output = Command::new("powershell").args([shell_command]).output().unwrap();
+        let output: tauri::core::process::Output = Command::new("powershell")
+            .args([shell_command])
+            .output()
+            .unwrap();
 
         sender.send(output.stdout.to_string()).unwrap();
     });
@@ -89,16 +93,13 @@ pub fn print_pdf (options: PrintOptions) -> String {
 
     // Receive the result from the spawned thread
     let result = receiver.recv().unwrap();
-    
-    
 
     if options.remove_after_print == true {
         let _ = remove_file(&options.path);
     }
-    
+
     return result;
 }
-
 
 /**
  * Get printer job on windows using powershell
@@ -116,12 +117,17 @@ pub fn get_jobs_by_id(printername: String, jobid: String) -> String {
     return output.stdout.to_string();
 }
 
-
 /**
  * Resume printers job on windows using powershell
  */
 pub fn resume_job(printername: String, jobid: String) -> String {
-    let output = Command::new("powershell").args([format!("Resume-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
+    let output = Command::new("powershell")
+        .args([format!(
+            "Resume-PrintJob -PrinterName \"{}\" -ID \"{}\" ",
+            printername, jobid
+        )])
+        .output()
+        .unwrap();
     return output.stdout.to_string();
 }
 
@@ -129,7 +135,13 @@ pub fn resume_job(printername: String, jobid: String) -> String {
  * Restart printers job on windows using powershell
  */
 pub fn restart_job(printername: String, jobid: String) -> String {
-    let output = Command::new("powershell").args([format!("Restart-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
+    let output = Command::new("powershell")
+        .args([format!(
+            "Restart-PrintJob -PrinterName \"{}\" -ID \"{}\" ",
+            printername, jobid
+        )])
+        .output()
+        .unwrap();
     return output.stdout.to_string();
 }
 
@@ -137,7 +149,13 @@ pub fn restart_job(printername: String, jobid: String) -> String {
  * pause printers job on windows using powershell
  */
 pub fn pause_job(printername: String, jobid: String) -> String {
-    let output = Command::new("powershell").args([format!("Suspend-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
+    let output = Command::new("powershell")
+        .args([format!(
+            "Suspend-PrintJob -PrinterName \"{}\" -ID \"{}\" ",
+            printername, jobid
+        )])
+        .output()
+        .unwrap();
     return output.stdout.to_string();
 }
 
@@ -145,6 +163,12 @@ pub fn pause_job(printername: String, jobid: String) -> String {
  * remove printers job on windows using powershell
  */
 pub fn remove_job(printername: String, jobid: String) -> String {
-    let output = Command::new("powershell").args([format!("Remove-PrintJob -PrinterName \"{}\" -ID \"{}\" ", printername, jobid)]).output().unwrap();
+    let output = Command::new("powershell")
+        .args([format!(
+            "Remove-PrintJob -PrinterName \"{}\" -ID \"{}\" ",
+            printername, jobid
+        )])
+        .output()
+        .unwrap();
     return output.stdout.to_string();
 }
